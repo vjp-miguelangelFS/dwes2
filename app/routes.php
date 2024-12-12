ca7ec4a156116ccc0aca51962d129fad82a4b7a0
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-12-12 19:15:27 +0100

    single-post solucionado

--- app/routes.php
+++ app/routes.php
@@ -6,7 +6,7 @@ $router->get('asociados', 'app/controllers/asociados.php');
 $router->get('blog', 'app/controllers/blog.php');
 $router->get('contact', 'app/controllers/contact.php');
 $router->get('imagenes-galeria', 'app/controllers/galeria.php');
-$router->get('post', 'app/controllers/single_post.ph');
+$router->get('post', 'app/controllers/single_post.php');
 $router->post('imagenes-galeria/nueva', 'app/controllers/nueva-imagen-galeria.php');
 $router->post('contacto-asociados/nuevo', 'app/controllers/nuevo-asociado.php');
 
