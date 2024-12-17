e3bf81ba6d4e03001a2d7f42e1988e4bdba1458a
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-12-17 20:14:05 +0100

    .

--- app/routes.php
+++ app/routes.php
@@ -9,5 +9,4 @@ $router->get('imagenes-galeria', 'app/controllers/galeria.php');
 $router->get('post', 'app/controllers/single_post.php');
 $router->post('imagenes-galeria/nueva', 'app/controllers/nueva-imagen-galeria.php');
 $router->post('contacto-asociados/nuevo', 'app/controllers/nuevo-asociado.php');
-
 $router->post('mensaje/nuevo', 'app/controllers/nuevo-mensaje.php');
